b0b2422527fb1e78b1762510a19c64a0e438b7b3
Himanshu Patel <himnshu@tulshi.co.uk>
2022-09-10 17:45:12 +0530

    Add project files.

---
+++ WebApp/Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Reflection;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public WeatherController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async  Task<IActionResult> Forecast()
+        {
+            List<WeatherForecastModel>? models;
+            using (var client = _httpClientFactory.CreateClient("APIEndPoint"))
+            {
+                models = await client.GetFromJsonAsync<List<WeatherForecastModel>>("/weatherforecast");                
+
+            }
+            return View(models);
+        }
+    }
+}
